b29dbf4e7b55b029c80d8fc31e78e9b82d036a44
Gimena Di Stefano Nicolina <gimedistefano@gmail.com>
2022-10-17 21:43:11 -0300

    agrego responsive y mejoras

---
+++ script.js
@@ -0,0 +1,19 @@
+//funcion para mostrar el Menu responsive
+
+function responsiveMenu(){
+    var x =document.getElementById("nav");
+    if (x.className===""){
+        x.className = "responsive";
+    }else{
+        x.className = "";
+    }
+}
+
+// function responsiveMenu(){
+//     var x =document.getElementById("redes");
+//     if (x.className===""){
+//         x.className = "responsive";
+//     }else{
+//         x.className = "";
+//     }
+// }
