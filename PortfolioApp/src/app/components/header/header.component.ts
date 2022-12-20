d0a7b7ed5c3e85adc0d6fca4c3df0af6c87c0b37
Gimena Di Stefano Nicolina <gimedistefano@gmail.com>
2022-12-19 23:53:03 -0300

    creacion json data

--- PortfolioApp/src/app/components/header/header.component.ts
+++ PortfolioApp/src/app/components/header/header.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  mostrar: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  mostrate():void{
+    this.mostrar=!this.mostrar;
+  }
 }
